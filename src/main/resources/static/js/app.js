51cf7c3ea1966505626c030681f9fc3c4ca01e23
hung.levan <hung.levan@live.com>
2023-09-14 14:05:17 +0700

    * [hunglv] Add Watch

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -25,7 +25,12 @@ stompClient.onConnect = (frame) => {
 	});
 	
 	stompClient.subscribe('/topic/fetchStatic', (record) => {
-		$('#static').html(record.body);
+	    const now = new Date();
+	    const hours = now.getHours().toString().padStart(2, '0');
+	    const minutes = now.getMinutes().toString().padStart(2, '0');
+	    const seconds = now.getSeconds().toString().padStart(2, '0');
+	    const currentTime = `${hours}:${minutes}:${seconds}`;
+		$('#static').html(record.body + ' | ' + ` ${currentTime}`  );
 	});
 };
 
@@ -82,3 +87,4 @@ function mapJsonToTable(idTable, jsonData) {
 		tbody.appendChild(tr);
 	});
 }
+
